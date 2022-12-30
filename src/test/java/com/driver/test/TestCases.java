cd4410ae1ab7e179484e2b7bb2398c30c930d759
Kanhaiya Mohan <kanhaiyamohan@Kanhaiyas-MacBook-Air.local>
2022-12-30 10:46:31 +0530

    Driver

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,4 +1,4 @@
-package com.driver;
+package com.driver.test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
